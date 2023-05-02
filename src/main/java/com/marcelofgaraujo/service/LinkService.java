efe33a8c9a53dac60195975def8ff0ae251992d4
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-05-02 11:39:16 -0300

    feat: save link method

---
+++ src/main/java/com/marcelofgaraujo/service/LinkService.java
@@ -0,0 +1,23 @@
+package com.marcelofgaraujo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.marcelofgaraujo.model.Link;
+import com.marcelofgaraujo.repository.LinkRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Service
+@Data
+@AllArgsConstructor
+public class LinkService {
+	
+	private LinkRepository linkRepository;
+	
+	public Link saveLink(Link link) {
+		linkRepository.save(link);
+		return link;
+	}
+	
+}
