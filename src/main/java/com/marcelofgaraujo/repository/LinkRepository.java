87018f3c41fd855f166392ebcbda0a1b764579c5
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-05-02 11:37:18 -0300

    feat: link jpa repository

---
+++ src/main/java/com/marcelofgaraujo/repository/LinkRepository.java
@@ -0,0 +1,11 @@
+package com.marcelofgaraujo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcelofgaraujo.model.Link;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+	
+	public Link findByShortURL(String shortURL);
+	
+}
